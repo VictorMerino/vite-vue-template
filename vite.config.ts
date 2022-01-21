e04cedbde4314732139230af41486bc40c5a3a9d
Victor Merino <victor.merinogarcia@gmail.com>
2022-01-21 22:31:50 +0100

    Add vitest (and basic config, alias and path included)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    deps: {
+      inline: ['@vue']
+    },
+    coverage: {
+      include: ['src/**/*.ts', 'src/**/*.d.ts', 'src/**/*.tsx', 'src/**/*.vue']
+    }
+  }
 })
